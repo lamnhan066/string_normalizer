0c269ed0e27bc8a66c11215ac3303f9ba4e87a74
lamnhan066 <hello@lamnhan.dev>
2023-12-23 14:10:27 +0700

    Update tests

--- test/string_normalizer_test.dart
+++ test/string_normalizer_test.dart
@@ -1,17 +1,30 @@
+import 'package:string_normalizer/src/crawler.dart';
 import 'package:string_normalizer/src/string_normalizer.dart';
 import 'package:test/test.dart';
 
 void main() {
-  final text = 'Đây là chữ có dấu';
+  test('Crawler', () async {
+    final crawler = Crawler();
+    final crawled = await crawler.crawl();
+
+    expect(crawled, isNotEmpty);
+  });
+
   test('Test static method', () {
-    final normalized = StringNormalizer.normalize(text);
+    final normalized = StringNormalizer.normalize('Đây là chữ có dấu');
 
     expect(normalized, equals('Day la chu co dau'));
   });
 
   test('Test extension', () {
-    final normalized = text.normalize();
+    final normalized = 'Đây là chữ có dấu'.normalize();
 
     expect(normalized, equals('Day la chu co dau'));
+  });
+
+  test('Specific test cases', () {
+    final normalized = 'ìáú'.normalize();
+
+    expect(normalized, equals('iau'));
   });
 }
