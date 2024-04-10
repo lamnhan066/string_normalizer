8bdb611056c4b7ae0affa974e2b9c7ddb02b011d
lamnhan066 <hello@lamnhan.dev>
2024-04-11 00:19:52 +0700

    Update example

--- example/main.dart
+++ example/main.dart
@@ -1,8 +1,12 @@
 import 'package:string_normalizer/string_normalizer.dart';
 
 void main(List<String> args) {
-  final text = 'Đây là chữ có dấu';
-  final normalized = StringNormalizer.normalize(text);
-  print(normalized);
-  print(text.normalize());
+  final text =
+      'Thîs Is à Löngêr Strîng Wîth Môre Cõmplicâtêd Cãses Änd Diãcritics. Αυτή είναι η ελληνική φράση με ειδικούς χαρακτήρες! Αυτό είναι το νούμερο 1234.';
+  final expectedText =
+      'this is a longer string with more complicated cases and diacritics. αυτη ειναι η ελληνικη φραση με ειδικους χαρακτηρες! αυτο ειναι το νουμερο 1234.';
+  final normalized = StringNormalizer.normalize(text).toLowerCase();
+  final normalizedExtension = text.normalize().toLowerCase();
+  print(normalized == expectedText); // true
+  print(normalizedExtension == expectedText); // true
 }
