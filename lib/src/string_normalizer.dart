0f11fb91cacbbbe878f20cd7716d894df4c52a9b
lamnhan066 <hello@lamnhan.dev>
2024-11-07 10:12:57 +0700

    Update the code to apply the flatted map

--- lib/src/string_normalizer.dart
+++ lib/src/string_normalizer.dart
@@ -1,7 +1,6 @@
 import 'package:characters/characters.dart';
 
 import 'data/local.dart';
-import 'utils.dart';
 
 /// StringNormalizer extension
 extension StringNormalizerE on String {
@@ -21,26 +20,21 @@ class StringNormalizer {
   // Internal.
   StringNormalizer._();
 
-  /// We do not need to initialize the data if it's not needed to use.
-  static Map<String, String>? _data;
-
   /// Normalize the [text].
   ///
   /// Convert all diacritical characters to ASCII characters.
   static String normalize(String text) {
-    _data ??= flatMap(data);
-
     StringBuffer result = StringBuffer();
 
     // Try to replace a whole character first then replace each code unit of a character.
     for (final char in text.characters) {
-      if (_data!.containsKey(char)) {
-        result.write(_data![char]);
+      if (data.containsKey(char)) {
+        result.write(data[char]);
       } else {
         final codeUnits = char.codeUnits;
         for (final codeUnit in codeUnits) {
           final char = String.fromCharCode(codeUnit);
-          result.write(_data![char] ?? char);
+          result.write(data[char] ?? char);
         }
       }
     }
